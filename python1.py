751ffd6b68d70850118bbea797c593ed48b1d5af
gifahriazharr <110508958+gifahriazharr@users.noreply.github.com>
2022-08-08 08:51:15 +0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#Belajar Output
+
+print("Hello World")
+print("nama saya Gifahri")
+print("saya berumur 15 tahun")
+print("saya tinggal di Depok")
+print("hobi saya menggambar")
+print("saya sekolah di SMK Harapan Bangsa")
+print("saya berasal dari SMPIT Rohimana")
+print("saya masuk jurusan TKJ")
+print("cita cita saya ingin menjadi pengusaha dan membahagiakan keluarga")
+print("saya suka makan gorengan")
---
+++ python2.py
@@ -0,0 +1,11 @@
+#Belajar Input
+
+nama = "Gifahri"
+umur = 15
+alamat = "Margonda, Kota Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
